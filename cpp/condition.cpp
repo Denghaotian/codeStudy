6c6d517c69dd173ce50b8ad542ec9f10fdc388d7
denghaotian <hifreddeng@gmail.com>
2020-11-04 02:39:51 -0500

    if

---
+++ cpp/condition.cpp
@@ -0,0 +1,31 @@
+
+//
+//  condition.cpp
+//  test
+//
+//  Created by 邓淏天 on 11/03/20.
+//  Copyright © 2020 邓淏天. All rights reserved.
+//
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main(){
+//选择结构
+    //if 单行if， 多行if， 多条件if
+    //单行if，用户输入分数，如果分数大于600，则视为考上，屏幕输出恭喜
+        int score = 0;
+        cout<<"请输入一个分数："<<endl;
+        cin>>score;
+        if(score>600)
+        {
+            cout<<"恭喜你考试一本大学"<<endl;
+        }
+    //多行if if else
+        
+
+
+    return 0;
+}
+
